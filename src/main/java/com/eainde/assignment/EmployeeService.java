33e2df734bf6646e7c59bc457cdc77ed515c6043
ManaalRehman <99686569+ManaalRehman@users.noreply.github.com>
2022-02-14 21:27:18 +0000

    Update EmployeeService.java
    Fails Test : Average . Note: Asserts 46800.09

--- src/main/java/com/eainde/assignment/EmployeeService.java
+++ src/main/java/com/eainde/assignment/EmployeeService.java
@@ -1,46 +1,123 @@
 package com.eainde.assignment;
 
+import com.eainde.assignment.entity.Address;
 import com.eainde.assignment.entity.Employee;
 import com.eainde.assignment.utility.FileUtil;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 public class EmployeeService {
 
     public List<Employee> getEmployees() throws IOException {
+    	
         FileUtil util= new FileUtil();
         List<String> employeeListInString=util.getFile();
+        List<Employee> employeeList = new ArrayList<Employee>();
 
-        // TODO write a code to get the list of employees from employeeListInString
-        // Employee should be in the same order as in the csv file
-        // Don't use any plugin like opencsv
-        return null;
+        //Removing table heads
+        employeeListInString.remove(0);
+        
+        for(String employee:employeeListInString) {
+        	
+        	String[] employeeDetails = employee.split(",");
+        	
+        	employeeList.add(new Employee(
+        			Long.parseLong(employeeDetails[0]), 
+        			employeeDetails[1], 
+        			Integer.parseInt(employeeDetails[2]), 
+        			Double.parseDouble(employeeDetails[3]), 
+        			employeeDetails[4], 
+        			new Address(employeeDetails[5],employeeDetails[6],employeeDetails[7],employeeDetails[8])));
+        }
+
+        return employeeList;
     }
-
-    public List<Employee> sortedEmployees(){
-        // TODO Sort an Employee Object list first by salary and if salaries are equal, then by name
-        return null;
+    
+	/*
+	 * Orders list by salary ascending order and name
+	 *
+	 */
+    
+    public List<Employee> sortedEmployees() throws IOException{
+ 
+    	List<Employee> sortedUsers = this.getEmployees().stream()
+    			  .sorted(Comparator.comparing(Employee::getName)) 
+    			  .sorted(Comparator.comparing(Employee::getSalary).reversed()) // add for decending.reversed()
+    			  .collect(Collectors.toList());
+    	
+        return sortedUsers;
     }
 
     public Employee getEmployeeById(long id){
-        // TODO write a code to get the employee by id
-        return null;
+
+    	
+    	Employee emp = null;
+    	
+	    try {
+	    	
+			for(Employee employee: this.getEmployees()) {
+				
+				if(employee.getId()==id) {
+					emp = employee;
+					break;
+				}
+				
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	  
+        return emp;
+    }
+    
+	/*
+	 * Calls sorted list, takes last element of list, which is employee of higest salary
+	 */
+    
+    public Employee getHighestSalaryEmployee() throws IOException{
+    	
+    	List<Employee> empList = this.sortedEmployees();
+        return empList.get(0);
     }
 
-    public Employee getHighestSalaryEmployee(){
-        // TODO write a code to get the employee who has highest salary
-        return null;
+    public int getAverageSalary() throws IOException{
+        
+    	OptionalDouble average = 
+        	    this.getEmployees().stream()
+        	              .map(Employee::getSalary)
+        	              .mapToDouble(a -> a)
+        	              .average();
+        
+       
+        return (int) average.getAsDouble();
     }
+    
 
-    public int getAverageSalary(){
-        // TODO write a code to get the average salary of employees
-        return 0;
-    }
+	/*
+	 * Add Employees from city to Hashmap and sort
+	 */
+    
+    public List<Employee> getEmployeesByCity(String city) throws IOException{
 
-    public List<Employee> getEmployeesByCity(String city){
-        // TODO write a code to get the employees by city. If there are two employees for one city then they should be sorted by employee name
-        // Note: use HashMap
-        return null;
+    	HashMap<String, Employee> hm = new HashMap<String, Employee>();
+   
+    	for(Employee e:this.getEmployees()) {
+    		
+    		if(e.getAddress().getCity().equals(city)) {
+    			hm.put(e.getName(), e);
+    		}
+    	}
+    	
+    	System.out.print(hm);
+
+        return new ArrayList<Employee>(hm.values());
+
     }
 }
